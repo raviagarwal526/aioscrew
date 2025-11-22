33851dc9774f43f9404eabf8fb96ef2e1c09baf4
jbandu <jbandu@gmail.com>
2025-11-22 08:53:55 -0600

    Updated db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(import.meta.env.DATABASE_URL || process.env.DATABASE_URL || '');
+const sql = neon(import.meta.env.VITE_DATABASE_URL || '');
 
 export { sql };
 
